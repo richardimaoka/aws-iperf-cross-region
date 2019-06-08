b4e2a3724b6d2d72854991e8068ddcf1e68852ab
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-06-08 17:01:45 +0900

    fix run tests

--- generate-ec2-input-json.sh
+++ generate-ec2-input-json.sh
@@ -3,7 +3,31 @@
 # cd to the current directory as it runs other shell scripts
 cd "$(dirname "$0")" || exit
 
-STACK_NAME="PingCrossRegionExperiment"
+#######################################################
+# Step 1: Parse options and error check
+#######################################################
+for OPT in "$@"
+do
+  case "$OPT" in
+    '--stack-name' )
+      if [ -z "$2" ]; then
+          echo "option --stack-name requires an argument -- $1" 1>&2
+          exit 1
+      fi
+      STACK_NAME="$2"
+      shift 2
+      ;;
+    -*)
+      echo "illegal option -- $1" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "${STACK_NAME}" ] ; then
+  >&2 echo "ERROR: Option --stack-name needs to be specified"
+  exit 1
+fi
 
 FILE_NAME=$(tempfile)
 
