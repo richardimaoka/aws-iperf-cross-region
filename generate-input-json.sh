cd6c709e4c28cc6e4858ee9bba5a3ea6fc4295a7
Richard Imaoka <richard.e.imaoka@gmail.com>
2019-07-11 23:01:18 +0900

    generate input

--- generate-input-json.sh
+++ generate-input-json.sh
@@ -4,49 +4,19 @@
 cd "$(dirname "$0")" || exit
 
 ######################################
-# 1.1 Parse options
-######################################
-for OPT in "$@"
-do
-  case "$OPT" in
-    '--instance-priority-file' )
-      if [ -z "$2" ]; then
-          echo "option --instance-priority-file requires an argument -- $1" 1>&2
-          exit 1
-      fi
-      INSTANCE_PRIORITY_FILE="$2"
-      shift 2
-      ;;
-    -*)
-      echo "illegal option $1" 1>&2
-      exit 1
-      ;;
-  esac
-done
-
-######################################
-# 1.2 Validate options
-######################################
-if [ -z "${INSTANCE_PRIORITY_FILE}" ] ; then
-  >&2 echo "ERROR: option --instance-priority-file needs to be passed"
-  exit 1
-fi
-
-######################################
 # 2. Generate input json files
 ######################################
 # sed to remove whitespace
-INSTANCE_TYPES=$(grep -v "#" < "${INSTANCE_PRIORITY_FILE}" | sed -e 's/\s//g')
+INSTANCE_TYPES=$(grep -v "#" < "instance-types.txt" | sed -e 's/\s//g' | grep -v "^$")
 
-for REGION in $(jq 'keys | .[]' cloudformation/output.json)
-do  AVAILABILITY_ZONE=$(jq -r ".${REGION}.availability_zone" cloudformation/output.json)
+for REGION in $(jq -r 'keys | .[]' cloudformation/output.json)
+do 
+  AVAILABILITY_ZONE=$(jq -r ".\"${REGION}\".availability_zone" cloudformation/output.json)
 
+  CHOSEN_INSTANCE_TYPE=""
   for INSTANCE_TYPE in $INSTANCE_TYPES
   do
-    CHOSEN_INSTANCE_TYPE=""
-    # jq "[\"key.with.dot\"][\"another.key.with.dot\"]" should be used for keys with dot  
-    if [ -n "${INSTANCE_TYPE}" ] && \
-       [ "true" = "$(jq ".[\"${AVAILABILITY_ZONE}\"][\"${INSTANCE_TYPE}\"]" aws-ec2-instance-types/output.json)" ] ; then
+    if [ "true" = "$(jq ".\"${AVAILABILITY_ZONE}\".\"${INSTANCE_TYPE}\"" aws-ec2-instance-types/output.json)" ] ; then
       CHOSEN_INSTANCE_TYPE=${INSTANCE_TYPE}
       break
     fi
@@ -56,15 +26,17 @@ do  AVAILABILITY_ZONE=$(jq -r ".${REGION}.availability_zone" cloudformation/outp
     >&2 echo "ERROR: none of instance types defined in file=${INSTANCE_PRIORITY_FILE} can be used in availability-zone=${AVAILABILITY_ZONE} of region=${REGION}"
     exit 1
   else
-    echo "${REGION} ${AVAILABILITY_ZONE} ${CHOSEN_INSTANCE_TYPE}"
+    echo "{ \"${REGION}\": { \"instance_type\": \"${CHOSEN_INSTANCE_TYPE}\" } }" > "intermediate/${REGION}.json"
   fi
 done
 
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-# TODO: generate json which adds instance_type to cloudformatoin/output.json and generate a new file
-
-
+######################################
+# 3. Aggregate json
+######################################
+OUTPUT_FILE=input.json
+cp "cloudformation/output.json" "${OUTPUT_FILE}"
+for JSON_FILE in intermediate/*.json
+do
+  OUTPUT=$(jq -s '.[0] * .[1]' "${OUTPUT_FILE}" "${JSON_FILE}")
+  echo "${OUTPUT}" > "${OUTPUT_FILE}"
+done
